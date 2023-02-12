07fbba50edd8611414d1121fc8d9a73bcf725f14
Marvin Hsu <marv.hsu@icloud.com>
2023-02-12 15:38:53 +0000

    add actix-web dependence and create health_check api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, web, HttpRequest, Responder, HttpResponse};
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(||{
+        App::new()
+        .route("/health_check", web::get().to(health_check))
+    })
+    .bind(("127.0.0.1",8080))?
+    .run()
+    .await
+}
+
+async fn health_check(_req: HttpRequest) -> impl Responder{
+    HttpResponse::Ok()
 }
