f4f725eb5402a0d519eee6bc866005d8c7bacd46
Marvin Hsu <marv.hsu@icloud.com>
2023-02-23 15:25:42 +0000

    health_check test

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use actix_web::{HttpServer, App, web, HttpRequest, Responder, HttpResponse};
-
+use zero2prod_2023::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(||{
-        App::new()
-        .route("/health_check", web::get().to(health_check))
-    })
-    .bind(("127.0.0.1",8080))?
-    .run()
-    .await
-}
-
-async fn health_check(_req: HttpRequest) -> impl Responder{
-    HttpResponse::Ok()
+    run().await
 }
